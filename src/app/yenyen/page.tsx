fee4a29b4b21925c5a5549d0e11c1e88bcb7726d
Ken Viriya <kenviriya.dev@gmail.com>
2025-09-07 04:11:03 +0700

    Adds motion library and improves contact page design
    Introduces the motion package and supporting dependencies for future animation needs.
    
    Updates contact page visuals with larger, bolder typography, a new profile image, and an enhanced call-to-action button for better user engagement and accessibility.

--- src/app/yenyen/page.tsx
+++ src/app/yenyen/page.tsx
@@ -6,9 +6,9 @@ import Link from 'next/link';
 
 const YenYenPage = () => {
   return (
-    <div className="flex flex-col items-center gap-6 py-10 bg-red-800">
-      <h1 className="text-2xl font-bold">Hubungi Kami</h1>
-      <h2 className="text-lg text-center">
+    <div className="flex flex-col items-center gap-6 py-10 bg-red-800 min-h-svh">
+      <h1 className="text-4xl font-bold">Hubungi Kami</h1>
+      <h2 className="text-2xl text-center">
         Order dan Konsultasi Aulora Pants melalui Whatsapp
       </h2>
 
@@ -16,14 +16,14 @@ const YenYenPage = () => {
         src={assets.profileYen}
         alt="Foto Profil Yen Yen"
         quality={100}
-        width={200}
-        height={250}
+        width={350}
+        height={437.5}
         className="rounded-lg"
       />
 
       <Link href="https://wa.me/628999524888">
-        <Button className="bg-green-600 py-5 px-10 text-white hover:bg-green-700 flex items-center gap-2">
-          <Phone className="w-6 h-6" /> Hubungi Yen Yen
+        <Button className="bg-[#166534] py-10 px-20 min-w-md text-white hover:bg-green-700 flex items-center gap-4 text-2xl font-semibold">
+          <Phone /> Hubungi Yen Yen
         </Button>
       </Link>
     </div>
